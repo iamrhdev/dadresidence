89b9b397477e4e9dcf6ad9ec7b6a20b8d3747534
unknown <rahilah@code.edu.az>
2024-03-21 12:55:59 +0400

    Footer Icons added

--- base/views.py
+++ base/views.py
@@ -7,9 +7,9 @@ def home(request):
     carousel_images = CarouselImage.objects.all()
     about_us = About.objects.get()
     our_services = OurService.objects.all()
-    room = Room.objects.all()
+    room_details = Room.objects.all()
     details_residence = DadResidenceDetail.objects.get()
-    context = {'carousel_images':carousel_images, 'about_us': about_us, 'our_services': our_services, 'room': room, 'details':details_residence}
+    context = {'carousel_images':carousel_images, 'about_us': about_us, 'our_services': our_services, 'room': room_details, 'details':details_residence}
     return render(request, 'base/home.html', context)
 
 
@@ -17,8 +17,9 @@ def about(request):
     about_video = AboutVideo.objects.get()
     about_us = About.objects.get()
     benefits = Benefit.objects.all()
+    details_residence = DadResidenceDetail.objects.get()
     extras = ExtraService.objects.all()
-    context = {'about_video':about_video, 'about_us':about_us, 'benefits': benefits, 'extras': extras}
+    context = {'about_video':about_video, 'about_us':about_us, 'benefits': benefits, 'extras': extras, 'details':details_residence}
     return render(request,'base/about.html',context)
 
 def room(request):
@@ -29,6 +30,5 @@ def room(request):
 def navbar(request):
     details_residence = DadResidenceDetail.objects.get()
     about = About.objects.get()
-    print("details_residence",details_residence)
     context = {'details':details_residence,'about_us':about}
     return render(request, 'navbar.html', context)
